1dc91f05b4b7315b96a35dd7e492fdfe20090f29
Kankaushik <kankaushik7@gmail.com>
2025-07-10 23:58:52 +0530

    Time: 36 ms (5.01%), Space: 12.6 MB (20.2%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        while "()" in s or "{}" in s or "[]" in s:
+            s = s.replace("()", "")
+            s = s.replace("{}", "")
+            s = s.replace("[]", "")
+        
+        return len(s) == 0
+        
